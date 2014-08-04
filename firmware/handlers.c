bb1aa61e6ec4d593a71a39082e27fcc57c23dc74
Dennis Muhlestein <djmuhlestein@gmail.com>
2014-08-04 14:55:04 -0600

    Update handlers for updated fx3 core. Ver 1.08

--- firmware/handlers.c
+++ firmware/handlers.c
@@ -6,12 +6,17 @@
 #include <spartan.h>
 #include "uxn1330_term.h"
 #include <fx3_term.h>
+#include <slfifo_handler.h>
 
 m24xx_config_t m24_config = { .dev_addr = TERM_FX3_PROM,
 			      .bit_rate = 400000,
 			      .size = 17, // 128KB prom
 };
 
+app_init_t app_init[] = {
+    { APP_INIT_VALID, slfifo_init, 0, 0, 0 },
+    { 0 }
+};
 
 io_handler_t io_handlers[] = {
   DECLARE_DUMMY_HANDLER(TERM_DUMMY_FX3),
