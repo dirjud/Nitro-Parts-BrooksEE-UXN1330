963e3221930ddfc31d430ea24323ecf0bf7e8368
Dennis Muhlestein <djmuhlestein@gmail.com>
2016-02-04 13:01:07 -0700

    correct max size of dram (64 megs). Add test debug info in case of failure.

--- py/UXN1330/tests/test_DRAM.py
+++ py/UXN1330/tests/test_DRAM.py
@@ -15,14 +15,18 @@ class TestDRAM(DevTestCase):
             Write random data to the dram.  Read back and verify data
             match.
         """
-        self.skipTest('Known fail case.')
-        size=32*1024*1024
+        size=16*1024*1024
         r=numpy.asarray(numpy.random.randint(2**32,size=size),dtype=numpy.uint32)
         #r=numpy.zeros((size,),dtype=numpy.uint32)
         self.dev.write('DRAM',0,r)
         buf=numpy.zeros( (size,), dtype=numpy.uint32)
         self.dev.read('DRAM',0,buf)
-        self.assertTrue ( (r==buf).all(), 'DRAM read data mismatch.')
+        bad=(buf-r).nonzero()[0]
+        if (len(bad)>0):
+            print "bad elements: %d" % len(bad)
+            for i in range(10 if len(bad)>10 else len(bad)):
+                print "buf[%d]=%08x r[%d]=%08x" % ( bad[i], buf[bad[i]], bad[i], r[bad[i]] )
+        self.assertTrue ( (r==buf).all(), 'DRAM read data mismatch' )
 
 
         
